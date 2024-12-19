a86fb8317230abcd7f86e19cff0ee548abd402c6
Ludaro1024 <waschmaschvanlu@gmail.com>
2024-12-19 09:56:32 +0100

    added info.md and debugdata

---
+++ client/garage/data.lua
@@ -0,0 +1,23 @@
+function WaitForGarageData()
+    while not Garage do
+        Wait(100)
+        -- function to check if the garage data is loaded from the server     
+    end
+end
+
+function RefreshGarageData()
+    -- ESX.TriggerServerCallback('l-garage:getGarages', function(garages)
+    --     Garage = garages
+    -- end)
+    -- example for later
+end
+
+function GetGarageData()
+    return Garage
+end
+
+function GetGarageDataByName(name)
+    return Garage[name]
+end
+
+
